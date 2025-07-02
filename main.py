0800812dd24a4ca19a9f89e42c245f985b857809
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-01 22:40:07 -0700

    add notes and comments

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ def main():
 
     client = genai.Client(api_key = api_key)
 
+#checks for prompts
     if len(sys.argv) < 2:
         raise Exception("There is no prompt,exiting with code 1")
         sys.exit(1)
 
-
+#starts content generation
     response = client.models.generate_content(
             model = 'gemini-2.0-flash-001',
             contents  = sys.argv[1]
         )
+
+#checks if there are commands    
     
     if len(sys.argv) < 3:
         print (response.text)
+
+#loops through arguments to find the command
     for arg in sys.argv: 
         if arg == "--verbose":
             print (f'User prompt: {sys.argv[1]}')
