1834c8e90f969ab2bc29991d3479c605f2e4b86d
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-01 22:37:54 -0700

    adding in verbose command

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import os
 import sys
 from dotenv import load_dotenv
+from google.genai import types
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main(): 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key = api_key)
+    client = genai.Client(api_key = api_key)
 
-if len(sys.argv) < 2:
-    raise Exception("There is no prompt,exiting with code 1")
-    sys.exit(1)
+    if len(sys.argv) < 2:
+        raise Exception("There is no prompt,exiting with code 1")
+        sys.exit(1)
 
-response = client.models.generate_content(
-        model = 'gemini-2.0-flash-001',
-        contents  = sys.argv[1]
-    )
-            
-print (response.text)
 
-print (f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print (f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    response = client.models.generate_content(
+            model = 'gemini-2.0-flash-001',
+            contents  = sys.argv[1]
+        )
+    
+    if len(sys.argv) < 3:
+        print (response.text)
+    for arg in sys.argv: 
+        if arg == "--verbose":
+            print (f'User prompt: {sys.argv[1]}')
+            print (f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print (f'Response tokens: {response.usage_metadata.candidates_token_count}')
+            print (response.text)
+    
+    
+if __name__ == "__main__":
+    main()
