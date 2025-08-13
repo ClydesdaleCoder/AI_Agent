cb95d7718c9e431a0d0a1f42329cb9c530f30a0a
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-08-13 09:18:25 -0700

    refactored main.py to function right

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google.genai import types
 from google import genai
-from functions.get_files_info import schema_get_files_info
-from functions.run_python import schema_run_python_file
-from functions.get_file_content import schema_get_files_content
+
+from call_function import available_functions
 
 system_prompt = """
 You are a helpful AI coding agent.
@@ -20,29 +19,36 @@ When a user asks a question or makes a request, make a function call plan. You c
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_run_python_file,
-        schema_get_files_content
-    ]
-)
-
 def main(): 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key = api_key)
 
+    verbose = '--verbose' in sys.argv
+
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith('--'):
+            args.append(arg)
+
 #checks for prompts
-    if len(sys.argv) < 2:
-        raise Exception("There is no prompt,exiting with code 1")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
+        
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
 #starts content generation
+
     response = client.models.generate_content(
             model = 'gemini-2.0-flash-001',
-            contents  = sys.argv[1], 
+            contents  = messages, 
             config=types.GenerateContentConfig(
                 tools=[available_functions],
                 system_instruction=system_prompt
@@ -50,20 +56,16 @@ def main():
         )
 
 #checks if there are commands    
+    if verbose:
+        print (f'User prompt: {user_prompt}')
+        print (f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print (f'Response tokens: {response.usage_metadata.candidates_token_count}')
     
-    if len(sys.argv) >= 3:
-        if response.candidates[0].content.parts[0].function_call:
-            function_call = response.candidates[0].content.parts[0].function_call
-            print (f'Calling function: {function_call.name}({function_call.args})')            
-    print (f'{response.text}')
-
-#loops through arguments to find the command
-    for arg in sys.argv: 
-        if arg == "--verbose":
-            print (f'User prompt: {sys.argv[1]}')
-            print (f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-            print (f'Response tokens: {response.usage_metadata.candidates_token_count}')
-            print (response.text)
+    if not response.function_calls:
+        print (response.text)
+    else:
+        for function_call_part in response.function_calls: 
+            print (f'Calling functions: {function_call_part.name}({function_call_part.args}')
     
     
 if __name__ == "__main__":
